63a7f4155f6f0b823ea09bdb9a74033e518ed2e4
hamidreza <h.r.mirtaheri@gmail.com>
2021-07-03 13:03:25 +0200

    hope this is correct ;)

--- hw/adctransmission.py
+++ hw/adctransmission.py
@@ -5,32 +5,32 @@ import datetime
 import time
 import threading
 import copy
-# import board
-# import busio
-# import adafruit_ads1x15.ads1015 as ADS
-# from adafruit_ads1x15.analog_in import AnalogIn
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
 
-access_token = ''
+access_token = 's2PUxOTsaqGbSeSxq9AV'
 
 headers = {'Content-Type': 'application/json',}
 
-IP, PORT =  '', ''
+IP, PORT =  'watt.linksfoundation.com', '8080'
 
 url_post = 'http://{}:{}/api/v1/{}/telemetry'.format(IP, PORT, access_token)
 
+i2c = busio.I2C(board.SCL, board.SDA)
 
+ads = ADS.ADS1015(i2c)
+
+channel_voltage = AnalogIn(ads, ADS.P0, ADS.P1)
 
 ctrl_flag = False
 send_flag = 0
 temp_controller = 0
 batch_voltages = np.array([])
-
-batch_voltages_bit = np.array([])
-
 batch_voltages_bits = np.array([])
 batch_currents = np.array([])
-
 tss=np.array([])
 
 # temporaries
@@ -48,57 +48,34 @@ def adc_read():
     global times
     global posts
     global batch_voltages
-
-    global batch_voltages_bit
-
     global batch_voltages_bits
     global batch_currents
-
     global tss
     global ctrl_flag
     global start_sampling_ts
     global end_sampling_ts
 
     voltages = np.array([])
-
-    voltages_bit = np.array([])
-
     voltages_bits = np.array([])
     currents = np.array([])
-
     sampling_rate = 0.01
-    send_batch_size =  100
-    raw_batch_size = 100
+    send_batch_size = 50
+    raw_batch_size = 1000
     try:
         start_sampling_ts = 0
         while True:
             if not start_sampling_ts:
                 start_sampling_ts = time.time()*1000
-
-
-            voltages = np.append(voltages, channel_voltage.voltage)
-            voltages_bit = np.append(voltages_bit, channel_voltage.value)
-            #currents = np.append(currents, np.random.random())
-
             voltages = np.append(voltages, np.random.random())
             voltages_bits = np.append(voltages_bits, np.random.random())
             currents = np.append(currents, np.random.random())
-
 
             if len(voltages) >= raw_batch_size:
                 end_sampling_ts = time.time()*1000
                 tss = (np.linspace(start_sampling_ts, end_sampling_ts,
                                    int(raw_batch_size/send_batch_size))).astype(np.int64)
 
-
                 batch_voltages = copy.deepcopy(voltages).reshape(-1, int(raw_batch_size/send_batch_size)).mean(axis=0)
-
-                batch_voltages_bit = copy.deepcopy(voltages_bit).reshape(-1, int(raw_batch_size/send_batch_size)).mean(axis=0)
-                send_flag = 1
-                start_sampling_ts = 0
-                voltages = np.array([])
-                voltages_bit = np.array([])
-
                 batch_voltages_bits = copy.deepcopy(voltages_bits).reshape(-1, int(raw_batch_size/send_batch_size)).mean(axis=0)
                 batch_currents = copy.deepcopy(currents).reshape(-1, int(raw_batch_size/send_batch_size)).mean(axis=0)
                 send_flag = 1
@@ -106,7 +83,6 @@ def adc_read():
                 voltages = np.array([])
                 voltages_bits = np.array([])
                 currents = np.array([])
-
 
             time.sleep(sampling_rate)
             temp_controller += 1
@@ -125,13 +101,13 @@ def http_write():
     try:
         while True:
             if send_flag:
-                #print(tss)
-                data = [dict(ts=str(tss[i]), values=dict(voltage_bit=str(batch_voltages_bit[i]), voltage=str(batch_voltages[i]))) for i in range(len(batch_voltages))]
-                
-                values=dict(current=str(batch_currents[i]),
-                            voltage_data_bits=str(batch_voltages_bits[i]),
-                            voltage=str(batch_voltages[i]))) for i in range(len(batch_voltages))]
-
+                print(tss)
+                data = [dict(ts=str(tss[i]),
+                    values=dict(
+                        current=str(batch_currents[i]),
+                        voltage_data_bits=str(batch_voltages_bits[i]),
+                        voltage=str(batch_voltages[i]))
+                    for i in range(len(batch_voltages))]
                 _message_to_send = json.dumps(data)
                 response = requests.post(url_post, headers=headers, data=_message_to_send)
                 send_flag = 0
